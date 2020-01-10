7f88d451ce52910906d8d1fd5185d8860232a2b1
Frank Panzer <frankp@cbm.local>
2020-01-10 15:37:35 +0100

    Tag 5
    MausEvents

---
+++ Tag5.js
@@ -0,0 +1,34 @@
+// Tag 5 - 10.01.2020
+// MausEvents	(In HTML Camelcase, in JavaScript alles klein)
+onmousemove		// Maus bewegen
+onmousedown		// Tastendruck
+onmouseup		// Maustaste loslassen
+onmousenter		// Maus betritt einen Bereich
+onmouseout		// Maus verlässt einen Bereich
+
+// Noch mal wiederholung 
+https://www.mediaevent.de/javascript/canvas.html
+
+// https://developer.mozilla.org/de/docs/Web/API/MouseEvent
+// https://www2.informatik.hu-berlin.de/Themen/www/selfhtml/javascript/sprache/eventhandler.htm
+
+// Math
+pow()		// Math.pow()		// 
+max()		// Math.max()		// Maximalwert
+min()		// Math.min()		// Minimalwert
+sin()		// Math.sin()		// Sinus
+sqrt()		// Math.sqrt()		// Quadratwurzel
+
+
+// Dreieck
+let a= 3;
+let b= 4;
+c = Math.sqrt( a * a + b * b )
+
+
+
+
+
+
+
+600x400x4=960.000	// Jedes Pixel besteht aus 4 Bytes (R,G,B,A)
